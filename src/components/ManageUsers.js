50c7f6aebe3eb3a719956610d22742f52dae9495
Debojit <sengupta.debojit07@gmail.com>
2020-09-22 17:23:36 +0530

    Edit modal

--- src/components/ManageUsers.js
+++ src/components/ManageUsers.js
@@ -4,16 +4,36 @@ import Axios from 'axios';
 import '../css/index.css';
 import '../css/bootstraptable.css';
 
+
 class ManageUser extends React.Component {
 
 	state = {
 		users:[],
-		user:{},
+		user:{
+		
+		},
+		id: '',
 		first_name: '',
 		last_name: '',
 		email: '',
 		address: ''
 	}
+
+	componentDidMount=()=>{
+		Axios.get("https://reqres.in/api/users").then(
+		   empResponse=>{
+			   this.setState({
+				  users:  empResponse.data.data
+			   });
+			   console.log(this.state.users);
+		   } 
+		)
+		   }
+changeUserIdHandler = event =>{
+this.setState({
+id: event.target.value
+			});
+			 }
  changeFirstNameHandler = event =>{
 this.setState({
 	first_name: event.target.value
@@ -36,11 +56,32 @@ changeAddressHandler = event =>{
 				address: event.target.value
 			});
 			 }
-
+	sendDataToEditUserModal = user =>{
+	console.log('req items:' + user.first_name);
+	 this.setState({
+		 id: user.id,
+		 first_name: user.first_name,
+		 last_name: user.last_name,
+		 email: user.email,
+		 address: user.address
+		
+	 });
+ }
+ clearState = () =>{
+	
+	 this.setState({
+		 id: '',
+		 first_name: '',
+		 last_name: '',
+		 email: '',
+		 address: ''
+		
+	 });
+ }
 			  
  handleAddUserSubmit = event =>{
 	event.preventDefault();
-	alert(`${this.state.first_name}  ${this.state.last_name}  ${this.state.email}`);
+	console.log(this.state.first_name,  this.state.last_name, this.state.email);
 	const addUserRequest = {
 		first_name: this.state.first_name,
 		last_name: this.state.last_name,
@@ -59,38 +100,52 @@ changeAddressHandler = event =>{
 	});
 	 }
 
-	componentDidMount=()=>{
-		Axios.get("https://reqres.in/api/users").then(
-		   empResponse=>{
-			   this.setState({
-				  users:  empResponse.data.data
-			   });
-			   console.log(this.state.users);
-		   } 
-		)
-		   }
+	 handleEditUserSubmit = event =>{
+		event.preventDefault();
+		console.log(this.state.first_name,  this.state.last_name, this.state.email);
+		const addUserRequest = {
+			first_name: this.state.first_name,
+			last_name: this.state.last_name,
+			email: this.state.email,
+			address: this.state.address
+		};
+		Axios.post("https://reqres.in/api/users",addUserRequest)
+		.then(response =>{
+			console.log(response);
+		})
+		this.setState({
+			first_name: '',
+			last_name: '',
+			email: '',
+			address: ''
+		});
+		 }
+
+	
 renderTableRecords = () =>{
 			return this.state.users.map((user,index)=>{
-				const {first_name,last_name,email } = user;
-				
+				const {id, first_name,last_name,email } = user;
+				console.log(id);
 				console.log(first_name);
 				console.log(last_name);
 				console.log(email);
 				return(
 					
-					<tr>
+					<tr key={index}>
 							<td>
 								<span className="custom-checkbox">
 									<input type="checkbox" id="checkbox1" name="options[]" value="1"></input>
 									<label for="checkbox1"></label>
 								</span>
 							</td>
+							<td>{id}</td>
 						<td>{first_name}</td>
 						<td>{last_name}</td>
 						<td>{email}</td>
 						<td>
-							<a href="#editUserModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-							<a href="#deleteUserModal" class="delete" data-toggle="modal" ng-click=""><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+						
+							<a href="#editUserModal" class="edit" data-toggle="modal" data-backdrop="static" onClick={()=>this.sendDataToEditUserModal(user)}><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+							<a href="#deleteUserModal" class="delete" data-toggle="modal" ><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
 							</td>
 </tr>
 					
@@ -101,7 +156,7 @@ renderTableRecords = () =>{
 
 			  
     render(){
-		const {first_name,last_name,email,address} = this.state
+		const {id,first_name,last_name,email,address} = this.state
     return(
 <div >
 <div className="container">
@@ -114,8 +169,8 @@ renderTableRecords = () =>{
 						</div>
 						<div className="col-xs-6">
 						
-							<a href="#addUserModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New User</span></a>
-							<a href="#deleteUserModal" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i> <span>Delete</span></a>						
+							<a href="#addUserModal" className="btn btn-success" data-toggle="modal"><i className="material-icons">&#xE147;</i> <span>Add New User</span></a>
+							<a href="#deleteUserModal" className="btn btn-danger" data-toggle="modal"><i className="material-icons">&#xE15C;</i> <span>Delete</span></a>						
 						</div>
 					</div>
 				</div>
@@ -128,6 +183,7 @@ renderTableRecords = () =>{
 									<label htmlFor="selectAll"></label>
 								</span>
 							</th>
+							<th>User Id</th>
 							<th>First Name</th>
 							<th>Last Name</th>
 							<th>Email</th>
@@ -155,63 +211,46 @@ renderTableRecords = () =>{
 		</div>  
     </div>
 	{/*<!-- Add User Modal HTML -->*/}
-	<div id="addUserModal" class="modal fade">
-		<div class="modal-dialog">
-			<div class="modal-content">
+	
+	<div id="addUserModal" className="modal fade">
+		<div className="modal-dialog">
+			<div className="modal-content">
 				<form>
-					<div class="modal-header">						
-						<h4 class="modal-title">Add Employee</h4>
-						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<div className="modal-header">						
+						<h4 className="modal-title">Add User</h4>
+						<button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 					</div>
-					<div class="modal-body">					
-						<div class="form-group">
+					<div className="modal-body">	
+					<div className="form-group">
+							<label>User Id</label>
+							<input type="text" value={id} onChange={this.changeUserIdHandler} className="form-control" required></input>
+						</div>				
+						<div className="form-group">
 							<label>First Name</label>
-							<input type="text" value={first_name} onChange={this.changeFirstNameHandler} class="form-control" required></input>
+							<input type="text" value={first_name} onChange={this.changeFirstNameHandler} className="form-control" required></input>
 						</div>
-						<div class="form-group">
+						<div className="form-group">
 							<label>Last Name</label>
-							<input type="text" value={last_name} onChange={this.changeLastNameHandler} class="form-control" required></input>
+							<input type="text" value={last_name} onChange={this.changeLastNameHandler} className="form-control" required></input>
 						</div>
-						<div class="form-group">
+						<div className="form-group">
 							<label>Email</label>
-							<input type="email" value={email} onChange={this.changeEmailHandler} class="form-control" required></input>
+							<input type="email" value={email} onChange={this.changeEmailHandler} className="form-control" required></input>
 						</div>
-						<div class="form-group">
+						<div className="form-group">
 							<label>Address</label>
-							<textarea class="form-control" value={address} onChange={this.changeAddressHandler} required></textarea>
+							<textarea className="form-control" value={address} onChange={this.changeAddressHandler} required></textarea>
 						</div>
 										
 					</div>
-					<div class="modal-footer">
-						<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel"></input>
-						<input type="submit" class="btn btn-success" value="Add" onClick={this.handleAddUserSubmit}></input>
+					<div className="modal-footer">
+						<input type="button" className="btn btn-default" data-dismiss="modal" value="Cancel"></input>
+						<input type="submit" className="btn btn-success" value="Add" onClick={this.handleAddUserSubmit}></input>
 					</div>
 				</form>
 			</div>
 		</div>
 	</div>  
-
-	{/*<!-- Delete Modal HTML -->*/}
-	<div id="deleteUserModal" class="modal fade">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<form>
-					<div class="modal-header">						
-						<h4 class="modal-title">Delete Employee</h4>
-						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					</div>
-					<div class="modal-body">					
-						<p>Are you sure you want to delete these Records?</p>
-						<p class="text-warning"><small>This action cannot be undone.</small></p>
-					</div>
-					<div class="modal-footer">
-						<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel"></input>
-						<input type="submit" class="btn btn-danger" value="Delete"></input>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
 
 	{/*<!-- Edit Modal HTML -->*/}
 	<div id="editUserModal" className="modal fade">
@@ -222,37 +261,59 @@ renderTableRecords = () =>{
 						<h4 className="modal-title">Edit User</h4>
 						<button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 					</div>
-					<div className="modal-body">					
+					<div className="modal-body">	
+					<div className="form-group">
+							<label>User Id</label>
+							<input type="text" value={id} onChange={this.changeFirstNameHandler} className="form-control" readOnly></input>
+						</div>				
 						<div className="form-group">
 							<label>First Name</label>
-							<input type="text" value={this.state.first_name} className="form-control" required></input>
+							<input type="text" value={first_name} onChange={this.changeFirstNameHandler} className="form-control" required></input>
 						</div>
 						<div className="form-group">
 							<label>Last Name</label>
-							<input type="text" value={this.state.user.last_name} className="form-control" required></input>
+							<input type="text" value={last_name}  onChange={this.changeLastNameHandler} className="form-control" required></input>
 						</div>
 						<div className="form-group">
 							<label>Email</label>
-							<input type="email" className="form-control" required></input>
+							<input type="email" value={email} onChange={this.changeEmailHandler} className="form-control" required></input>
 						</div>
 						<div className="form-group">
 							<label>Address</label>
-							<textarea className="form-control" required></textarea>
+							<textarea className="form-control"  onChange={this.changeAddressHandler} value={address} required></textarea>
 						</div>
-						<div className="form-group">
-							<label>Phone</label>
-							<input type="text" className="form-control" required></input>
-						</div>					
+					</div>
+					<div className="modal-footer">
+						<input type="button" className="btn btn-default" onClick={()=>this.clearState()} data-dismiss="modal" value="Cancel"></input>
+						<input type="submit" className="btn btn-info" value="Save" onClick={this.handleEditUserSubmit}></input>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	{/*<!-- Delete Modal HTML -->*/}
+	<div id="deleteUserModal" class="modal fade">
+		<div className="modal-dialog">
+			<div className="modal-content">
+				<form>
+					<div className="modal-header">						
+						<h4 className="modal-title">Delete Employee</h4>
+						<button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					</div>
+					<div className="modal-body">					
+						<p>Are you sure you want to delete these Records?</p>
+						<p class="text-warning"><small>This action cannot be undone.</small></p>
 					</div>
 					<div className="modal-footer">
 						<input type="button" className="btn btn-default" data-dismiss="modal" value="Cancel"></input>
-						<input type="submit" className="btn btn-info" value="Save"></input>
+						<input type="submit" className="btn btn-danger" value="Delete"></input>
 					</div>
 				</form>
 			</div>
 		</div>
 	</div>
 </div>
+
     );
   }
   }
